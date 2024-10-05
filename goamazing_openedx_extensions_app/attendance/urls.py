32767794db4bb6f39eb5b0d238c18b727a3235a6
Vu Nguyen <vu.nguyen@dmttek.com>
2024-10-05 14:00:46 +0700

    test

--- goamazing_openedx_extensions_app/attendance/urls.py
+++ goamazing_openedx_extensions_app/attendance/urls.py
@@ -3,6 +3,6 @@ from .views import attendance_get, attendance_post
 from django.conf import settings
 
 urlpatterns = [
-    path('courses/{settings.COURSE_KEY_PATTERN}', attendance_get, name='attendance_get'),  # for GET requests
-    path('courses/{settings.COURSE_KEY_PATTERN}', attendance_post, name='attendance_post'),  # for POST requests
+    path(fr'courses/{settings.COURSE_KEY_PATTERN}', attendance_get, name='attendance_get'),  # for GET requests
+    path(fr'courses/{settings.COURSE_KEY_PATTERN}', attendance_post, name='attendance_post'),  # for POST requests
 ]
